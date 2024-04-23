298adfb4233e5e542dc0c36871493dc0fbe2081b
marques-rick <hmservicostech@outlook.com.br>
2024-04-22 22:40:36 -0300

    created mock table

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  courses: Course[];
+  displayedColumns: string[] = ['name', 'category'];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.courses = [{ _id: '1', name: 'Angular', category: 'Front-end' }];
   }
 
+  ngOnInit(): void {}
 }
